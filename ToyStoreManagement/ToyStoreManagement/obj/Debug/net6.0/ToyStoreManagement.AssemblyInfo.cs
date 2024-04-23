aef44f1029633ed8c5b4065c729571de4150f3df
Minh Nguyên <108600635+Nguyennm080503@users.noreply.github.com>
2024-04-23 09:09:42 +0700

    add gitignore

--- ToyStoreManagement/ToyStoreManagement/obj/Debug/net6.0/ToyStoreManagement.AssemblyInfo.cs
+++ ToyStoreManagement/ToyStoreManagement/obj/Debug/net6.0/ToyStoreManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToyStoreManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bce4b284040fda3317032862b664de430e56dde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToyStoreManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToyStoreManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
