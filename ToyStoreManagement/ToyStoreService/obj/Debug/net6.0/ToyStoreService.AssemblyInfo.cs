5df2cdb018789bba80d86dc1ee9f252bed41b3b2
Minh Nguyên <108600635+Nguyennm080503@users.noreply.github.com>
2024-04-23 09:44:32 +0700

    Revert "add gitignore"
    This reverts commit aef44f1029633ed8c5b4065c729571de4150f3df.

--- ToyStoreManagement/ToyStoreService/obj/Debug/net6.0/ToyStoreService.AssemblyInfo.cs
+++ ToyStoreManagement/ToyStoreService/obj/Debug/net6.0/ToyStoreService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToyStoreService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb515e279379348374008801fbea8ed87be34c1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToyStoreService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToyStoreService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
