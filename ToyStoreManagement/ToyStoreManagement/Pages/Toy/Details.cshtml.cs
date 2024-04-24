8e3d04ba7eed8e02ef9289a397fa0c1cac7a9930
taila034 <tailase151035@fpt.edu.vn>
2024-04-24 21:34:32 +0700

    Update add to cart in Product detail

--- ToyStoreManagement/ToyStoreManagement/Pages/Toy/Details.cshtml.cs
+++ ToyStoreManagement/ToyStoreManagement/Pages/Toy/Details.cshtml.cs
@@ -9,6 +9,7 @@ using BusinessObjects.Models;
 using ToyStoreService.Interface;
 using ToyStoreManagement.View_Models;
 using ToyStoreService.Implement;
+using Newtonsoft.Json;
 
 namespace ToyStoreManagement.Pages.Toy
 {
@@ -28,7 +29,7 @@ namespace ToyStoreManagement.Pages.Toy
             _categoryService = categoryService;
         }
 
-        public Product Product { get; set; } = default!; 
+        public Product Product { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -42,11 +43,68 @@ namespace ToyStoreManagement.Pages.Toy
             {
                 return NotFound();
             }
-            else 
+            else
             {
                 Product = product;
             }
             return Page();
+        }
+        public async Task<IActionResult> OnPostAddToCartAsync([FromForm] int productId)
+        {
+            Product product = _service.GetProductById(productId);
+            //Check validation for product
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            if (product.StockQuantity == 0)
+            {
+                ViewData["Messsage"] = "This product is not enough in stock.";
+                return RedirectToPage();
+            }
+
+            List<CartItem> cart = HttpContext.Session.GetComplexData<List<CartItem>>("CART");
+            CartItem item = new CartItem
+            {
+                ProductId = product.ProductId,
+                ProductName = product.Name,
+                Price = (decimal)product.Price,
+                Quantity = 1,
+            };
+            if (cart != null)
+            {
+                if (cart.Exists(i => i.ProductId == product.ProductId))
+                {
+                    // If item is already exist in cart
+                    int index = cart.FindIndex(i => i.ProductId == product.ProductId);
+                    if (product.StockQuantity < cart[index].Quantity + 1)
+                    {
+                        TempData["Message"] = "The product is not enough in stock";
+                    }
+                    else
+                    {
+                        cart[index].Quantity += 1;
+                        cart[index].Price += (decimal)product.Price;
+                        TempData["Message"] = "Add product to cart successfully";
+                    }
+                }
+                else
+                {
+                    cart.Add(item);
+                    TempData["Message"] = "Add product to cart successfully";
+                }
+                HttpContext.Session.SetComplexData("CART", cart);
+            }
+            else //When cart is null -> create new cart
+            {
+                List<CartItem> list = new List<CartItem>();
+                list.Add(item);
+                HttpContext.Session.SetComplexData("CART", list);
+                TempData["Message"] = "Add product to cart successfully";
+            }
+
+            return RedirectToPage("./Index");
         }
     }
 }
