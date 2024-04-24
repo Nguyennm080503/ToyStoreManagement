57111968e8beb5c40757027d40a270aa0b02aa52
OwlNightist <saxdau@gmail.com>
2024-04-24 15:51:38 +0700

    fear toy Details

--- ToyStoreManagement/ToyStoreManagement/Pages/Toy/Details.cshtml.cs
+++ ToyStoreManagement/ToyStoreManagement/Pages/Toy/Details.cshtml.cs
@@ -6,28 +6,38 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using BusinessObjects.Models;
+using ToyStoreService.Interface;
+using ToyStoreManagement.View_Models;
+using ToyStoreService.Implement;
 
 namespace ToyStoreManagement.Pages.Toy
 {
     public class DetailsModel : PageModel
     {
-        private readonly BusinessObjects.Models.ToyStoreDBContext _context;
+        private readonly IProductService _service;
+        private readonly ICategoryService _categoryService;
 
-        public DetailsModel(BusinessObjects.Models.ToyStoreDBContext context)
+        [BindProperty(SupportsGet = true)]
+        public int ProductId { get; set; }
+        public string Message { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+        public DetailsModel(IProductService service, ICategoryService categoryService)
         {
-            _context = context;
+            _service = service;
+            _categoryService = categoryService;
         }
 
-      public Product Product { get; set; } = default!; 
+        public Product Product { get; set; } = default!; 
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null || _context.Products == null)
+            if (id == null || _service.GetAllProducts == null)
             {
                 return NotFound();
             }
 
-            var product = await _context.Products.FirstOrDefaultAsync(m => m.ProductId == id);
+            var product = _service.GetProductById((int)id);
             if (product == null)
             {
                 return NotFound();
